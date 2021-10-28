24b3a48f756a7a790854a1fc29f66c0c9485fb4e
富永康太 <tominagakouta@tominagakoutanoMac-mini.local>
2021-10-28 09:04:28 +0900

    commit

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,29 @@ import { VitePWA } from 'vite-plugin-pwa'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    vue(), VitePWA()
+    vue(),
+    VitePWA({
+      manifest: {
+        "lang": "ja",
+        "name": "ひつじノート",
+        "short_name": "ひつじノート",
+        "description": "訪問した人の情報と訪問内容を楽しく管理できるアプリです。",
+        "theme_color": "##fff9f3",
+        "background_color": "#fff9f3",
+        "display": "standalone",
+        "icons": [
+          {
+            "src": "/pwa-192x192.png",
+            "sizes": "192x192",
+            "type": "image/png"
+          },
+          {
+            "src": "/pwa-512x512.png",
+            "sizes": "512x512",
+            "type": "image/png"
+          }
+        ]
+      }
+    })
   ]
 })
